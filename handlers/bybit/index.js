a2bb607729d43f703bc36045199af386625c0133
VIP <asus@LAPTOP-2CUASV32.localdomain>
2022-11-04 22:45:31 +0800

    Updated added new endpoint /spot-order

--- handlers/bybit/index.js
+++ handlers/bybit/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const { longPosition, shortPosition, orderPosition } = require("./order.perp");
+const { sportOrderPosition } = require("./order.spot");
 
 const bybitRouter = express.Router();
 
 bybitRouter.post("/long", longPosition);
 bybitRouter.post("/short", shortPosition);
 bybitRouter.post("/order", orderPosition);
+bybitRouter.post('/spot-order', sportOrderPosition);
 
 module.exports = bybitRouter;
---
+++ handlers/bybit/order.spot.js
@@ -0,0 +1,56 @@
+const { SpotClientV3 } = require('bybit-api');
+
+const _KEY = process.env.BYBIT_KEY;
+const _SECRET = process.env.BYBIT_SECRET;
+const testnet = process.env.NODE_ENV == "development";
+
+const sportOrderPosition = async (req, res) => {
+    const restClient = new SpotClientV3({
+        key: _KEY,
+        secret: _SECRET,
+        testnet
+    });
+    let symbol = String(req.body.symbol.join('')).toUpperCase();
+    let orderQty = String(req.body.qty);
+    let primaryCurrency = String(req.body.symbol[0]);
+    let secondaryCurrency = String(req.body.symbol[1]);
+    const side = String(req.body.side).charAt(0).toUpperCase() + String(req.body.side).slice(1);
+    const orderType = String(req.body.orderType).toUpperCase();
+    const position = {
+        symbol,
+        orderQty,
+        side,
+        orderType
+    };
+
+    try {
+
+        const balances = await restClient.getBalances();
+        const currencyBalance = balances.result.balances.filter(item => side==="Sell"?item.coinId === primaryCurrency:item.coinId===secondaryCurrency).pop();
+
+        console.log(currencyBalance);
+
+        if(req.body.allBalance) {
+            position.orderQty = currencyBalance.free;
+        }
+
+        const response = await restClient.submitOrder(position);
+        if(response.retMsg !== "OK") {
+            console.log(`[${new Date().toGMTString()}]: ${req.method.toUpperCase()} ${req.url} ${400} -> ${response.retMsg}`);
+            return res.status(400).json(response);
+        }
+        console.log(`[${new Date().toGMTString()}]: ${req.method.toUpperCase()} ${req.url} ${200} -> ${response.retMsg}`);
+        return res.status(200).json(response);
+
+    } catch(error) {
+
+        console.log(`[${new Date().toGMTString()}]: ${req.method.toUpperCase()} ${req.url} ${400} -> Server error: ${error.message}`);
+        return res.status(400).json(error);
+    }
+}
+
+
+
+module.exports = {
+    sportOrderPosition
+};
